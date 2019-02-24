714b56bb3e9544396b7952d9fbcb8f1c925481e4
nayebare <mnayebare@rwanda.cmu.edu>
2019-02-25 00:07:42 +0300

    fron-end validation done

--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,31 @@
 /* Main app js file */
 
 $(document).ready(function () {
+
   $('.add-row').click(function () {
     var inputData = {};
     var number1 = $('#number1').val();
-    action = $('#action').val();
-    number2 = $('#number2').val();
-    expected = 0;
+        action = $('#action').val();
+        number2 = $('#number2').val();
+        expected = 0;
+
     // put values in array
     inputData.opd1 = number1;
     inputData.opd2 = number2;
+    inputData.opn = action;
 
-    // input fields should not be empty
+if(number1 == '' || number2 == '' ){alert('number1 or number2 is empty');} else{
 
-    //if no option is selected  rise an error
-
-
+ if(action == 'no'){alert('please select operation');} else{
+ 
     // addition option
     if (action == 'add') {
       $.post('/api/add', inputData, function (expected) {
         var response = (Math.ceil(parseInt(number1)) + Math.ceil(parseInt(number2)));
         if (response != expected) {
           passed = 'no';
-          var markupHight = "<tr><td><input type='checkbox' name='record'></td><td>" + number1 + '</td><td>' + number2 + '</td><td>' + expected + '</td><td>' + response + '</td><td>' + passed + '</td></tr>';
-          $('table tbody').append(markup);
+          var markupHighlight = "<tr><td><input type='checkbox' name='record'></td><td>" + number1 + '</td><td>' + number2 + '</td><td>' + expected + '</td><td>' + response + '</td><td>' + passed + '</td></tr>';
+          $('table tbody').append(markupHighlight);
         } else {
           passed = 'yes';
           var markup = "<tr backgroud-color='lime-gren'><td><input type='checkbox' name='record'></td><td>" + number1 + '</td><td>' + number2 + '</td><td>' + expected + '</td><td>' + response + '</td><td>' + passed + '</td></tr>';
@@ -86,16 +88,22 @@ $(document).ready(function () {
       }, 'JSON');
 
     }
+  }
+}
 
-  });
-
+  });// end main function
+  
   // Find and remove selected table rows
   $('.delete-row').click(function () {
-    
     $('table tbody').find('input[name="record"]').each(function () {
       if ($(this).is(':checked')) {
+        var result = confirm("Are you sure  you want to delete this item?");
         $(this).parents('tr').remove();
       }
+      else {
+        alert('please select item');
+      }
     });
+
   });
 });
